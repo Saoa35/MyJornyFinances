aaa782dad5a7aba294813d7a97563d8718640c0f
Saoa35 <saoamah@gmail.com>
2023-08-10 14:24:10 +0400

    * cleaned code

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StatusBar, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export default App;
