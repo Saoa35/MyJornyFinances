f0c1d6ba3a1baa945ad2cf0f5ad85f1ff5d95599
Saoa35 <saoamah@gmail.com>
2023-08-11 18:52:24 +0400

    + added costs.js

--- src/components/CostsCart.jsx
+++ src/components/CostsCart.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import {Text, View, FlatList} from 'react-native';
 import {EmptyList} from './EmptyList';
-import {items} from '../data/items';
+import {costs} from '../data/costs';
 
 export const CostsCart = () => {
   return (
     <FlatList
-      data={items}
-      numColumns={2}
+      data={costs}
       ListEmptyComponent={<EmptyList />}
       keyExtractor={item => item.id}
       showsVerticalScrollIndicator={false}
-      columnWrapperStyle={{
-        justifyContent: 'space-around',
-      }}
       renderItem={({item}) => {
         return (
-          <View item={item}>
-            <Text>Costs Cart</Text>
+          <View>
+            <View>
+              <Text>{item.title}</Text>
+              <Text>{item.category}</Text>
+            </View>
+            <View>
+              <Text>$ {item.amount}</Text>
+            </View>
           </View>
         );
       }}
---
+++ src/data/costs.js
@@ -0,0 +1,20 @@
+export const costs = [
+  {
+    id: 1,
+    title: 'bought shoes',
+    amount: 300,
+    category: 'shopping',
+  },
+  {
+    id: 2,
+    title: 'ate pizza',
+    amount: 15,
+    category: 'food',
+  },
+  {
+    id: 3,
+    title: 'take a city tour',
+    amount: 120,
+    category: 'entertainment',
+  },
+];
