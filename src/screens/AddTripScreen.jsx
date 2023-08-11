7470dc600e39778db32c9807f1f0af5671da7283
Saoa35 <saoamah@gmail.com>
2023-08-11 14:20:30 +0400

    + added Add trip button

--- src/screens/AddTripScreen.jsx
+++ src/screens/AddTripScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, Image, View, TextInput} from 'react-native';
+import {Text, Image, View, TextInput, TouchableOpacity} from 'react-native';
 import styled from 'styled-components/native';
 import {ScreenWrapper} from './HomeScreen';
 import {colors} from '../theme';
@@ -50,6 +50,41 @@ const DirectionText = styled.Text`
   font-size: 18px;
   line-height: 28px;
   font-weight: bold;
+  margin-left: 25px;
+  margin-bottom: 10px;
+`;
+
+const TextInputWrapper = styled.View`
+  margin: 8px 8px 0 8px;
+`;
+
+const ContinentTextInput = styled.TextInput`
+  padding: 10px 20px;
+  background-color: white;
+  border-radius: 9999px;
+  margin-bottom: 12px;
+`;
+
+const CountryTextInput = styled.TextInput`
+  padding: 10px 20px;
+  background-color: white;
+  border-radius: 9999px;
+  margin-bottom: 12px;
+`;
+
+const AddTripOpacity = styled.TouchableOpacity`
+  background-color: ${colors.button};
+  margin: 24px 8px;
+  padding: 12px;
+  border-radius: 9999px;
+  box-shadow: 1px 2px rgb(0, 0, 0, 0.05);
+`;
+
+const AddTripText = styled.Text`
+  text-align: center;
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
 `;
 
 export const AddTripScreen = () => {
@@ -64,15 +99,19 @@ export const AddTripScreen = () => {
           <ImageWrapper>
             <AddTripImage source={require('../assets/images/4.png')} />
           </ImageWrapper>
-          <View>
+          <TextInputWrapper>
             <DirectionText>Wich Continent ?</DirectionText>
-            <TextInput />
+            <ContinentTextInput />
             <DirectionText>Which Country ?</DirectionText>
-            <TextInput />
-          </View>
+            <CountryTextInput />
+          </TextInputWrapper>
         </AddTripContainer>
 
-        <View></View>
+        <View>
+          <AddTripOpacity>
+            <AddTripText>Add Trip</AddTripText>
+          </AddTripOpacity>
+        </View>
       </AddTripWrapper>
     </ScreenWrapper>
   );
